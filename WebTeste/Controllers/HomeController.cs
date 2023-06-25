606e9d94c4e977b4aaed494a6335de72f892b573
Gustavo73 <gustavoferreira73@live.com>
2023-06-25 19:40:38 -0300

    Adicionar arquivos de projeto.

---
+++ WebTeste/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebTeste.Models;
+using WebTeste.Service;
+using System.Security.Claims;
+
+namespace WebTeste.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [AllowAnonymous] //Indica que esse metodo pode ser acessado sem esta logado
+        public IActionResult Index(bool erroLogin)
+        {
+            if (erroLogin)
+            {
+                ViewBag.Erro = "Nome ou senha errado!";
+            }
+
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+               return RedirectToAction("Proficional");
+            }
+
+            return View(); 
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(Usuario user)
+        {
+
+            var usuarioDB = new Usuario()
+            {
+                Nome = "Gustavo",
+                Senha = "123456",
+                Cargo = ""
+            };
+
+            if (!usuarioDB.Nome.Equals(user.Nome)||
+                !usuarioDB.Senha.Equals(user.Senha)
+                )
+            {
+                return RedirectToAction("Index", new {erroLogin = true});
+            }
+
+            await new Services().Login(HttpContext, user);
+            return RedirectToAction("Proficional");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Sair()
+        {
+            await new Services().Logoff(HttpContext);
+            return RedirectToAction("Index");
+        }
+
+        [Authorize(Roles = "adm, teste")]
+        public IActionResult Proficional()
+        {
+            ViewBag.Permissoes = HttpContext.User.Claims.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value);
+
+            return View();
+        }
+
+    }
+}
---
+++ WebTeste/Service/Services.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using WebTeste.Models;
+
+namespace WebTeste.Service
+{
+    public class Services
+    {
+        //Metodo para logar
+
+        public async Task Login(HttpContext ctx ,Usuario user)
+        {
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, user.Nome));
+            claims.Add(new Claim(ClaimTypes.Role, user.Cargo));
+
+            var claimsIdentuty = new ClaimsPrincipal(new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme));
+
+            var authProperties = new AuthenticationProperties
+            {
+                ExpiresUtc = DateTime.Now.AddHours(8),
+                IssuedUtc = DateTime.Now
+            };
+
+            await ctx.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsIdentuty, authProperties);
+        }
+
+        //Metodo de remoção
+
+        public async Task Logoff(HttpContext ctx)
+        {
+            await ctx.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+    }
+}
